cb674bd236c6195230676743e02a7d94ad658bf3
Lysdora <lysdora.web@gmail.com>
2023-07-14 09:56:38 +0200

    add try catch and reload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let questionCalc;
 let firstNumber;
 let secondNumber;
 let result;
+let restart;
 
 // Bloc function
 function add(firstNumber, secondNumber) {
@@ -15,61 +16,73 @@ function multiply(firstNumber, secondNumber) {
   return result;
 }
 
-function substraction(num1, num2) {
-  let result = num1 - num2;
+function substraction(firstNumber, secondNumber) {
+  let result = firstNumber - secondNumber;
   return result;
 }
 
-function divide(num1, num2) {
-  let result = num1 / num2;
-  if (num2 == 0) {
-    alert("Cannot divide num2 by 0.");
+function divide(firstNumber, secondNumber) {
+  let result = firstNumber / secondNumber;
+  if (secondNumber == 0) {
+    throw new Error(`Cannot divide ${secondNumber} by 0.`);
   } else {
     return result;
   }
 }
+if (!restart) {
+  alert("Hooray 👾 ! Welcome to the magical world of numbers and operations!");
+}
 
 // message welcome
-alert("Hooray ! Welcome to the magical world of numbers and operations!");
 
 // Bloc do while
 do {
   questionCalc = +prompt(
     `What would you like to do?
-     1 - Addition
-     2 - Multiplication
-     3 - Subtraction
-     4 - Division`
+     1 🟡 Addition
+     2 🟢 Multiplication
+     3 🟣 Subtraction
+     4 🟠 Division`
   );
 } while (![1, 2, 3, 4].includes(questionCalc));
 
 do {
-  firstNumber = +prompt("Choose a first number");
-  secondNumber = +prompt("Choose a second number");
+  firstNumber = +prompt("Choose a first number 🦒");
+  secondNumber = +prompt("Choose a second number 🐧");
 } while (isNaN(firstNumber) || isNaN(secondNumber));
 
 // switch
-switch (questionCalc) {
-  case 1:
-    result = add(firstNumber, secondNumber);
+try {
+  switch (questionCalc) {
+    case 1:
+      result = add(firstNumber, secondNumber);
+      break;
 
-    break;
-  case 2:
-    result = multiply(firstNumber, secondNumber);
+    case 2:
+      result = multiply(firstNumber, secondNumber);
+      break;
 
-    break;
-  case 3:
-    result = substraction(firstNumber, secondNumber);
+    case 3:
+      result = substraction(firstNumber, secondNumber);
+      break;
 
-    break;
-  case 4:
-    result = divide(firstNumber, secondNumber);
+    case 4:
+      result = divide(firstNumber, secondNumber);
+      break;
 
-    break;
-  default:
-    break;
+    default:
+      throw new Error(`An error has occurred 🔥`);
+  }
+  alert(`The result is : ${result} 🐼`);
+} catch (error) {
+  alert(error);
 }
 
-alert(`The result is : ${result}`);
+restart = confirm("Do you like try again 🙉?");
+if (restart) {
+  window.location.reload();
+  alert("Hello again 😀");
+} else {
+  alert("Bye bye See you again! 🦊");
+}
 // end
-alert("Bye bye See you again!");
